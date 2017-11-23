edbf55e03821a19bb2fa9a31aff3897eba6198ff
Tohir <thohiru.omoloye@talentbase.ng>
2017-11-23 14:52:38 +0000

    Solid principles

---
+++ Solid/lab9.js
@@ -0,0 +1,26 @@
+class Engine{
+    constructor(){
+        this.numbber = ''
+        this.size = 0
+        this.totalRunningTime = 0
+    }
+}
+
+class Vehicle{
+    constructor(){
+        this.chassisnumber = ''
+        this.owner = ''
+        this.engine = ''
+        this.odometer = ''
+        this.numberOfWheels = ''
+    }
+}
+
+class Person{
+    constructor(){
+        this.name = ''
+        this.address = ''
+        this.canDrive = false
+        this.readpoetry = false
+    }
+}
