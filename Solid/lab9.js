7b8996a9fa91d2dfe67116b905b451a3af00298c
Tohir <thohiru.omoloye@talentbase.ng>
2017-11-23 19:11:14 +0000

    lab 9 on oop

--- Solid/lab9.js
+++ Solid/lab9.js
@@ -1,26 +1,69 @@
 class Engine{
-    constructor(){
-        this.numbber = ''
-        this.size = 0
-        this.totalRunningTime = 0
+    constructor(number, size, totalRunningTime){
+        this.number = number
+        this.size = size
+        this.totalRunningTime = totalRunningTime
+    }
+
+    engineInfo(){
+        console.log("\nEngine No: " + this.number + "\nLitres of Fuel: " + this.size + "\nRunning Time: " + this.totalRunningTime + " km/h")
+    }
+
+    runningTime(){
+        console.log(this.totalRunningTime + " km/h")
     }
 }
 
-class Vehicle{
-    constructor(){
-        this.chassisnumber = ''
-        this.owner = ''
-        this.engine = ''
-        this.odometer = ''
-        this.numberOfWheels = ''
+class Vehicle {
+
+    constructor(obj){
+        this.chassisnumber = obj.chassis
+        this.owner = obj.owner
+        this.distance = obj.distance
+        this.runtime = obj.runtime
+
+        this.details = []
+    }
+
+    findOwner(){
+        let fs = require('fs')
+        // read external file
+        let chatLogs = JSON.parse(fs.readFileSync('carsOOP.json', 'utf8'))
+        this.details = chatLogs.filter(x => x.chassis === this.chassisnumber) 
+    }
+
+    getTotalRunningTime(){
+        let fs = require('fs')
+        // read external file
+        let chatLogs = JSON.parse(fs.readFileSync('carsOOP.json', 'utf8'))
+        var find = chatLogs.filter(x => x.chassis === this.chassisnumber) 
+        //get total runnung time
+        
+        console.log("Total Running Time", find.reduce((sum, find) => sum + find.runtime, 0))
     }
 }
 
 class Person{
-    constructor(){
-        this.name = ''
-        this.address = ''
-        this.canDrive = false
-        this.readpoetry = false
+
+    constructor(name, address){
+        this.name = name
+        this.address = address
+        // this.readpoetry = false
     }
+
+    drive(){
+        console.log("driving...")
+    }
+
 }
+
+var object = {
+    "chassis": "J9O 5P3",
+    "owner": "Alexis",
+    "distance": "22.4078718509",
+    "runtime": 90
+}
+// console.log(object)
+
+let vehicle = new Vehicle(object)
+vehicle.getTotalRunningTime()
